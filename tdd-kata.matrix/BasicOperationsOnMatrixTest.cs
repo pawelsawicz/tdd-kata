16cdcdc9f894ea33471ae49ef60485840210217b
pawelsawicz <pawel@sawicz.eu>
2014-05-05 23:43:12 +0200

    custom exception has been created, and validation has been created while adding two matrix

--- tdd-kata.matrix/BasicOperationsOnMatrixTest.cs
+++ tdd-kata.matrix/BasicOperationsOnMatrixTest.cs
@@ -22,23 +22,45 @@ namespace tdd_kata.matrix
             Assert.AreEqual(expectedMatrix, result);
         }
 
+        [Test]
+        public void Given2DimensionalMatrixButSizeIsDifferentThenAdd()
+        {
+            int[,] firstMatrix = { { 1, 2, 3 }, { 1, 2, 3 } };
+            int[,] secondMatrix = { { 1, 2 }, { 5, 6 } };
+
+            var exceptionResult = Assert.Throws<DiffrentSizeOfMarix>(() => firstMatrix.Add(secondMatrix));
+
+        }
+
+    }
+
+    public class DiffrentSizeOfMarix : Exception
+    {
+         
     }
 
     public static class MatrixHelpers
     {
         public static int[,] Add(this int[,] firstMatrix, int[,] secondMatrix)
         {
-            int[,] result = new int[firstMatrix.GetLength(0), firstMatrix.GetLength(1)];
-
-            for (int i = 0; i < firstMatrix.GetLength(0); i++)
+            if (firstMatrix.GetLength(0) != secondMatrix.GetLength(0) || firstMatrix.GetLength(1) != secondMatrix.GetLength(1))
             {
-                for (int j = 0; j < firstMatrix.GetLength(1); j++)
-                {
-                    result[i, j] = firstMatrix[i, j] + secondMatrix[i, j];
-                }
+                throw new DiffrentSizeOfMarix();
             }
+            else
+            {
+                int[,] result = new int[firstMatrix.GetLength(0), firstMatrix.GetLength(1)];
 
-            return result;
+                for (int i = 0; i < firstMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < firstMatrix.GetLength(1); j++)
+                    {
+                        result[i, j] = firstMatrix[i, j] + secondMatrix[i, j];
+                    }
+                }
+
+                return result;
+            }
         }
     }
 }

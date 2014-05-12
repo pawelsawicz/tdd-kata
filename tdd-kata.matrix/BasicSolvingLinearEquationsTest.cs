b5044d9e7111a0f2d7dde5083690bd094a36bc05
pawelsawicz <pawel@sawicz.eu>
2014-05-12 12:05:18 +0200

    refactor cramer method

--- tdd-kata.matrix/BasicSolvingLinearEquationsTest.cs
+++ tdd-kata.matrix/BasicSolvingLinearEquationsTest.cs
@@ -12,46 +12,37 @@ namespace tdd_kata.matrix
         [Test]
         public void GivenTwoLinearEquationsThenSolveItByCramerAndReturnValue()
         {
+            //arrange
             int[,] equationToSolve = { { 4, 4, 4 }, { 4, 8, 8 }, { 4, 8, 12 } };
             int[] values = { 0, -4, -16 };
             int[] expectedVariables = { 1, 2, -3 };
 
-            int[] result = new int[equationToSolve.GetLength(0)];
-            int mainDeterminant = equationToSolve.Determinant();
-            int xDeterminant = 0;
-            int[,] xMatrix = { { 4, 4, 4 }, { 4, 8, 8 }, { 4, 8, 12 } };
-            int yDeterminant = 0;
-            int[,] yMatrix = { { 4, 4, 4 }, { 4, 8, 8 }, { 4, 8, 12 } };
-            int zDeterminant = 0;
-            int[,] zMatrix = { { 4, 4, 4 }, { 4, 8, 8 }, { 4, 8, 12 } };
+            //act
+            int[] result = CramerSolve(equationToSolve, values);                      
 
-            for (int i = 0; i < equationToSolve.GetLength(1); i++)
-            {
-                xMatrix[i, 0] = values[i];
-            }
-
-            xDeterminant = xMatrix.Determinant();
-
-            for (int i = 0; i < equationToSolve.GetLength(1); i++)
-            {
-                yMatrix[i, 1] = values[i];
-            }
-
-            yDeterminant = yMatrix.Determinant();
-
-            for (int i = 0; i < equationToSolve.GetLength(1); i++)
-            {
-                zMatrix[i, 2] = values[i];
-            }
-
-            zDeterminant = zMatrix.Determinant();
-
-            result[0] = xDeterminant / mainDeterminant;
-            result[1] = yDeterminant / mainDeterminant;
-            result[2] = zDeterminant / mainDeterminant;
-
+            //assert
             Assert.AreEqual(expectedVariables, result);
+        }
 
+        private int[] CramerSolve(int[,] equationToSolve, int[] values)
+        {
+            var result = new int[equationToSolve.GetLength(0)];
+            int mainDeterminant = equationToSolve.Determinant();
+
+            for (int i = 0; i < equationToSolve.GetLength(0); i++)
+            {
+                int[,] matrix = new int[equationToSolve.GetLength(0), equationToSolve.GetLength(1)];
+                Array.Copy(equationToSolve, matrix, equationToSolve.Length);                
+
+                for (int j = 0; j < equationToSolve.GetLength(1); j++)
+                {
+                    matrix[j,i] = values[j];
+                }
+
+                result[i] = matrix.Determinant() / mainDeterminant;
+            }
+
+            return result;
         }
     }
 }
